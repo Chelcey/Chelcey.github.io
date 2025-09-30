110ff985bce174887731705eeab4be1f38449aea
Chelcey <71199896+Chelcey@users.noreply.github.com>
2025-09-29 20:17:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 document.querySelectorAll('.swiper').forEach(slider => {
+        const nav = slider.nextElementSibling;
+        
         new Swiper(slider, {
                 direction: 'horizontal',
                 loop: true,
                 pagination: {
                         el: slider.querySelector('.swiper-pagination'),
                         type: 'progressbar'
-                },
+                        },
                 navigation: {
-                        nextEl: slider.querySelector('.swiper-button-next'),
-                        prevEl: slider.querySelector('.swiper-button-prev'),
+                        nextEl: nav.querySelector('.swiper-button-next'),
+                        prevEl: nav.querySelector('.swiper-button-prev'),
                 },
         });
 });
